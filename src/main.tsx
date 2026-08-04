d40b8efac3324e0ad83290c3d0254b562052adfd
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-08-04 13:38:17 -0400

    feat: add AuthProvider for Firebase authentication state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+
 import App from './App.tsx';
 import './App.css';
-import { BrowserRouter } from 'react-router';
+
+import AuthProvider from '@/features/auth/providers/AuthProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </BrowserRouter>
     </StrictMode>
 );
