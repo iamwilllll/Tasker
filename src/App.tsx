07895e350b44fa3bd6c21cea4fc8fe98c118dff4
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-08-07 12:42:27 -0400

    feat: implement authentication routes with GuestRoute and ProtectRoute components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { Routes, Route } from 'react-router';
+import { Login, Register } from '@/features/auth/pages';
 import { TaskDashboard } from '@/features/task/pages';
+import { ProtectRoute, GuestRoute } from '@/components';
+import { useAuth } from '@/features/auth/hooks';
 
 function Home() {
     return <h2>Start page</h2>;
 }
 
 export default function App() {
+    const { isAuthenticated, isAuthLoading } = useAuth();
+
+    if (isAuthLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route element={<GuestRoute />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
 
-            <Route path="/dashboard" element={<TaskDashboard />} />
+            <Route element={<ProtectRoute isAuthenticated={isAuthenticated} />}>
+                <Route path="/dashboard" element={<TaskDashboard />} />
+            </Route>
         </Routes>
     );
 }
