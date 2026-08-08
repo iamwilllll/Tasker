a6c62d9d09243eaa5352fc6d7f6e7a0194078bd3
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-08-08 17:34:56 -0400

    feat: define UserT type with user properties and preferences

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,26 @@
+import type { User } from 'firebase/auth';
+import type { FieldValue } from 'firebase/firestore';
+
 export type CustomErrorResponse = {
     message: string;
     code: string;
     originalError: unknown;
     error: boolean;
+};
+
+export type UserT = {
+    uid: User['uid'];
+    name: string;
+    email: string;
+    photoURL: User['photoURL'];
+    provider: 'google' | 'password';
+
+    createdAt: FieldValue;
+    updatedAt: FieldValue;
+
+    preferences?: {
+        theme?: 'light' | 'dark' | 'shiny';
+        language?: 'en' | 'es';
+        notifications?: boolean;
+    };
 };
